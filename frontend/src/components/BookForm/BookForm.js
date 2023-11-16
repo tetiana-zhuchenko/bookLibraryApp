86b38ee15cc38e63c3a079fbf9f0cc5c717abcee
Tanya <taniyhamail@gmail.ua>
2023-11-16 13:14:19 +0200

    feat: added thunk func

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import axios from 'axios'
-import { addBook } from '../../redux/slices/booksSlice'
+import { addBook, thunkFunction } from '../../redux/slices/booksSlice'
 import booksData from '../../data/books.json'
 import { createBookWithId } from '../../utils/createBookWithId'
 import './BookForm.css'
@@ -37,15 +36,8 @@ const BookForm = () => {
     }
   }
 
-  const handleAddRandomBookViaAPI = async () => {
-    try {
-      const res = await axios.get('http://localhost:4000/random-book')
-      if (res?.data?.author && res?.data?.title) {
-        dispatch(addBook(createBookWithId(res.data, 'API')))
-      }
-    } catch (error) {
-      console.log(error)
-    }
+  const handleAddRandomBookViaAPI = () => {
+    dispatch(thunkFunction)
   }
 
   return (
--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { createBookWithId } from '../../utils/createBookWithId'
 
 const initialState = []
 
@@ -34,6 +36,17 @@ const booksSlice = createSlice({
 })
 
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions
+
+export const thunkFunction = async (dispatch, getState) => {
+  try {
+    const res = await axios.get('http://localhost:4000/random-book')
+    if (res?.data?.author && res?.data?.title) {
+      dispatch(addBook(createBookWithId(res.data, 'API')))
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 export const selectBooks = (state) => state.books
 
